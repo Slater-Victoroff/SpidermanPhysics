845fe7ae3ec6ca6f108c3c25046b66660ea3344c
Abe Levitan <allevitan@gmail.com>
2013-04-16 17:13:50 -0400

    3Dsim now uses a real ode solver, and is 7 orders of magnitude better.

--- 3Dsim.py
+++ 3Dsim.py
@@ -1,37 +1,54 @@
-from __future__ import division
 from visual import *
 import numpy as np
 from numpy import linalg as la
+from scipy.integrate import ode
 from euler import euler
-from time import clock
+from time import clock, sleep
 
-params = {}
-params['eqlen'] = 1 #m
-params['m'] = 1 #kg
-params['k'] = 100 #N/m
-params['g'] = np.matrix((0,-9.81,0)) #m/s^2
+#Some parameters for the simulation - this way, they only get defined once
+class Params(object):
+    def __init__(self):
+        self.eq = 1 #m
+        self.m = 1 #kg
+        self.k = 100 #N/m
+        self.g = np.array((0,-9.81,0)) #m/s^2
 
-def springPendulum(inlist, params):
-    r = np.matrix(inlist[0:3])
-    v = np.matrix(inlist[3:])
+# The function that implements the spring pendulum odes
+def spodes(t, v, p):
+    r = v[0:3]
+    v = v[3:]
     drdt = v
-    dvdt = params['g'] + params['k']*(params['eqlen']/la.norm(r) - 1)*r
-    out = drdt.tolist()[0] + dvdt.tolist()[0]
-    #Because it's a matrix, returns a list of lists.
+    dvdt = p.g + r*p.k*(p.eq/la.norm(r) - 1)
+    out = np.concatenate((drdt, dvdt))
     return out
 
-def visualize(vals, params):
+#A function that will update the vpython sim
+def visualize(vals, p):
     r = vals[0:3]
     v = vals[3:]
     rod.axis=r
     ball.pos=r
-    # For energy checking
-    print params['m']/2 * la.norm(v)**2 + params['k']/2*(la.norm(r)-params['eqlen'])**2 - params['m'] * np.dot(params['g'],r)
+    # Printing the current total energy
+    print p.m/2 * la.norm(v)**2 + p.k/2*(la.norm(r)-p.eq)**2 - p.m * np.dot(p.g,r)
 
-simspring = (lambda l: springPendulum(l,params))
-vis = (lambda l: visualize(l,params))
+#Instantiating the parameters
+params = Params()
 
-dt = 0.001
+#Defining sp as a function only of t and v
+sp = (lambda t,v: spodes(t,v,params))
+
+#Setting up the vpython stuff
 rod = cylinder(pos=(0,0,0), axis=(1,0,0), radius=0.05)
 ball = sphere(pos=(1,0,0), radius = 0.1)
-output = euler([1,0,0,0,0,0],simspring, visualize=vis, exittime=10, dt=dt, realtime=True)
+
+# creating the sim object, dopri5 is basically ode45
+sim = ode(sp).set_integrator('dopri5')
+sim.set_initial_value([1,0,0,0,0,0],0)
+endtime = 10
+dt = 0.01
+
+#This follows the form of the example on the scipy site
+while sim.successful() and sim.t < endtime:
+    sim.integrate(sim.t+dt)
+    sleep(dt)
+    visualize(sim.y, params)
