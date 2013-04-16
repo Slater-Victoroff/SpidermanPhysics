6f59a28706ae0384868471a4070b44a7ff18f9ec
Abe Levitan <allevitan@gmail.com>
2013-04-16 14:13:55 -0400

    Add a forward euler function that approximates ode45

---
+++ euler.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import numpy as np
+from numpy import linalg as la
+
+def euler(inlist, iterfunc, exitfunc=(lambda vals: True),\
+          exittime=float('infinity'), dt=0.01):
+    """ This function takes a tuple of initial conditions and a function
+    that will output the derivatives of each input, and will perform a
+    simulation until the cows come home (or an optional exit time or set
+    of exit conditions are met)"""
+    time = 0
+    output = [inlist]
+    while exitfunc(output[-1]) and time < exittime:
+        output.append([x + dxdt*dt for (x,dxdt) in \
+                       zip(output[-1],iterfunc(output[-1]))])
+        time += dt
+    return output
+
+if __name__ == '__main__':
+    iterfunc = (lambda x: x)
+    inlist = (1,)
+    output = euler(inlist, iterfunc, exittime=1, dt=0.001)
+    print "e = %f" %output[-1][0]
