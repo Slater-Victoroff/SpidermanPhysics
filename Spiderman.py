7c53f8b72743482baeeafd2276d5453d7b1c9c69
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-05-09 07:02:40 -0400

    Gross Code

--- Spiderman.py
+++ Spiderman.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 import ThreeDsim
 import cProfile, pstats, io
 from PIL import Image
-
 import json
-
 
 """Worth noting that x is distance along the width of the street,
 y is vertical, and z is distance along the length of the street."""
@@ -15,7 +13,7 @@ class Params(object):
         self.mass = 50 #kg
         self.k = k #N/m
         self.area = 0.5 #m^2
-        self.extension = 0.84 #Percentage of new length
+        self.extension = extension #Percentage of new length
         self.gravity = np.array((0,-9.81,0)) #m/s^2
         self.airDensity = 1.2 #kg/m^3
         self.webDensity = 0.0001 #kg/m Assuming a constant thickness for web
@@ -29,6 +27,13 @@ class Params(object):
         self.energyTracker = []
         self.kinetic = []
         self.potential = []
+
+def energyCalculator(velocity, r, parameters):
+    kinetic = 0.5*parameters.mass*(la.norm(velocity)**2)
+    potential = -parameters.mass * np.dot(parameters.gravity,r)
+    d = la.norm(r)-parameters.equilibriumLength
+    spring = (parameters.k/2*(d)**2 if d > 0 else 0)
+    return (kinetic, potential, spring, (kinetic+potential+spring))
 
 def goGetEmTiger():
     """The top-level optimization function. Currently is foiled by
@@ -55,7 +60,11 @@ def simplewhen(vec, params):
     else:
         return True
 
-def websling(r0=np.array([12,0,0]), v0=np.array([0,0,10]), where=simplewhere, when=simplewhen, iterations=50, k=1000, extension = 0.84, vis=False):
+def eqwhen(vec, params):
+    r, v = vec[:3], vec[3:]
+    return energyCalculator(v,r, params)[3] > params.initialEnergy
+
+def websling(r0=np.array([12,0,0]), v0=np.array([0,0,10]), where=simplewhere, when=simplewhen, iterations=50, k=1000, extension = 0.84, vis=False, plot=False):
     """Simulates Spiderman's webslinging for the given parameters"""
     params = Params(k=k, extension=extension)
     # the where function takes a position and velocity in global space
@@ -66,50 +75,117 @@ def websling(r0=np.array([12,0,0]), v0=np.array([0,0,10]), where=simplewhere, wh
     t = 0
     if vis: ThreeDsim.initVisualization(rGlobal, params)
     else: ThreeDsim.initFalseVisualization(rGlobal, params)
+    (r0new, l) = where(v0[-1], rGlobal[-1], params)
+    r0.append(r0new)
+    params.equilibriumLength = la.norm(r0[-1])
+    params.initialEnergy = energyCalculator(v0[-1],r0[-1],params)[3]
     for i in xrange(0, iterations):
-        (r0new, l) = where(v0[i], rGlobal[i], params)
-        r0.append(r0new)
-        print rGlobal[i] - r0[i]
-        params.rod.pos = rGlobal[i] - r0[i]
         params.equilibriumLength = l
         t, outvec = ThreeDsim.switchingSpring(
             t, np.concatenate((r0[i],v0[i])),params, when, vis)
         rf, v0new = outvec[:3], outvec[3:]
         v0.append(v0new)
         rGlobal.append(rGlobal[i] - r0[i] + rf)
+        params.initialEnergy = energyCalculator(v0[-1],r0[-1],params)[3]
         params.left = not params.left
-    #plt.plot(params.energyTracker)
-    # plt.savefig('fig.png')
+        (r0new, l) = where(v0[i], rGlobal[i], params)
+        r0.append(r0new)
+        params.rod.pos = rGlobal[i] - r0[i]
+    if plot:
+        plt.plot(params.energyTracker)
+        plt.savefig('fig.png')
     return rGlobal, v0, params.energyTracker, t
 
-def potentialLossFunction(k, extension):
-    rGlobal, v0, energyTracker, t = websling(np.array([12,0,0]),np.array([0,0,10]), simplewhere, simplewhen, 50, k, extension)
-    potentialLoss = (energyTracker[0][1] - energyTracker[-1][1])/len(energyTracker)
-    return potentialLoss
+def singleIter(r0=np.array([-12,-10,-10]), v0=np.array([0,0,10]), params=Params(), when=eqwhen, vis=False):
+    """Simulates Spiderman's webslinging for the given parameters"""
+    # the where function takes a position and velocity in global space
+    # and returns radius to spiderman from the new pendulum center,
+    # along with the equilibrium length of that web.
+    t = 0
+    #trick it into thinking there's no initial stretchiness
+    params.equilibriumLength = la.norm(r0)
+    params.initialEnergy = energyCalculator(v0,r0,params)[3]
+    params.equilibriumLength = la.norm(r0) * params.extension
+    t, outvec = ThreeDsim.switchingSpring(
+         t, np.concatenate((r0,v0)),params, when, vis)
+    rf, vf = outvec[:3], outvec[3:]
+    return rf, vf, params.energyTracker, t
 
-def manualPhasePlot(function, ranges, granularities):
+def plotify(r0=np.array([-12,-10,-10]), v0=np.array([0,0,10]), plot=True, k=1000, extension=0.84, filename='fig.png'):
+    """Simulates Spiderman's webslinging for the given parameters"""
+    params = Params(k=k, extension=extension)
+    print params.extension
+    v0, rf, energy, t = singleIter(r0,v0, params, eqwhen, vis=False)
+    if plot:
+        plt.plot(energy)
+        plt.savefig(filename)
+    return rf, v0, energy, t
+
+
+def searchFor(condition, ranges, granularities, r0=[-12,-10,-10], v0=np.array([0,0,10]), when=eqwhen):
+    params = Params()
     xSpan = ranges[0][1]-ranges[0][0]
     ySpan = ranges[1][1] - ranges[1][0]
     xTrend = int(xSpan/granularities[0])
     yTrend = int(ySpan/granularities[1])
-    fullPhasePlot = []
+    goodlist = []
     for y in range(0,yTrend):
-        currentRow = []
+        params.k = ranges[1][0] + y*granularities[1]
+        for x in range(0,xTrend):
+            params.extension = ranges[0][0]+x*granularities[0]
+            stats = singleIter(r0, v0, params)
+            print stats
+            if condition(stats, r0, v0):
+                goodlist.append((params.k, params.extension))
+                print 'Good data!'
+    return goodlist
+
+
+def lowDU(stats, r0, v0):
+    return abs(r0[1] - stats[0][1]) < 0.001
+
+def lossFunction(params, k, extension):
+    params.k = k
+    params.extension = extension
+    rGlobal, v0, energyTracker, t = singleIter(params=params)
+    potentialLoss = (energyTracker[0][1] - energyTracker[-1][1])/len(energyTracker)
+    return potentialLoss, t
+
+    
+def normalize(array):
+    maximum = np.max(array)
+    minimum = np.min(array)
+    array -= minimum
+    array *= 255*(minimum/maximum)
+
+def manualPhasePlot(function, ranges, granularities):
+    params = Params()
+    xSpan = ranges[0][1]-ranges[0][0]
+    ySpan = ranges[1][1] - ranges[1][0]
+    xTrend = int(xSpan/granularities[0])
+    yTrend = int(ySpan/granularities[1])
+    potentialPhasePlot=[]
+    timePhasePlot = []
+    for y in range(0,yTrend):
+        potentialRow = []
+        timeRow = []
         usefulY = ranges[1][0] + y*granularities[1]
         for x in range(0,xTrend):
             usefulX = ranges[0][0]+x*granularities[0]
-            print usefulX, usefulY
-            currentRow.append(function(usefulX,usefulY))
-        fullPhasePlot.ap;pend(currentRow)
-    fullPhasePlot = np.array(fullPhasePlot)
-    print fullPhasePlot
+            stats = function(params, usefulX, usefulY)
+            potentialRow.append(stats[0])
+            timeRow.append(stats[1])
+        potentialPhasePlot.append(potentialRow)
+        timePhasePlot.append(timeRow)
+    potentialPhasePlot = np.array(potentialPhasePlot)
+    timePhasePlot = np.array(timePhasePlot)
+    normalize(potentialPhasePlot)
+    normalize(timePhasePlot)
     #Normalizing
-    maximum = np.max(fullPhasePlot)
-    minimum = np.min(fullPhasePlot)
-    fullPhasePlot -= minimum
-    fullPhasePlot *= 255*(minimum/maximum)
-    im = Image.fromarray(fullPhasePlot,'L')
-    im.save("phasePlot.png")
+    potImage = Image.fromarray(potentialPhasePlot,'L')
+    potImage.save("potentialPhasePlot.png")
+    timeImage = Image.fromarray(timePhasePlot, 'L')
+    timeImage.save("timePhasePlot.png")
 
 def cramAwayAllData(function, ranges, granularities):
     xSpan = ranges[0][1]-ranges[0][0]
@@ -128,12 +204,24 @@ def cramAwayAllData(function, ranges, granularities):
         pickle.dump(fullPhasePlot,jsonData)
 
 if __name__ == '__main__':
-    #statify=True
-    #if statify:
-    #    pr = cProfile.Profile()
-    #    pr.enable()
-    manualPhasePlot(potentialLossFunction, np.array([[1000,5000],[0.6,0.9]]), np.array([100,0.0075]))
-    #cramAwayAllData(websling, np.array([[1000,5000],[0.6,0.9]]), np.array([100,0.0075]))
-    #if statify:
-    #    pr.disable()
-    #    pstats.Stats(pr).print_stats()
+    statify=False
+    if statify:
+        pr = cProfile.Profile()
+        pr.enable()
+    #rGlobal, v0, E, t = websling(np.array([12,0,0]),np.array([0,0,10]), simplewhere, eqwhen, 10, vis=True, plot=True)
+    #rGlobal, v0, E, t = plotify(plot=True)
+    #print searchFor(lowDU, np.array([[500,2000],[0.8,1.0]]), np.array([37.5,.005]))
+    params=Params()
+    potentialChanges = []
+    granularity = 20
+    for change in range (0,100):
+        print change
+        potentialChanges.append(lossFunction(params,(300+change*25), 0.84))
+    plt.plot(potentialChanges)
+    plt.show()
+    #plotify(k=362.5, extension=0.90, plot=True, filename='fig4.png')
+    #manualPhasePlot(lossFunction, np.array([[500,2000],[0.8,1.0]]), np.array([37.5,.005]))
+    #cramAwayAllData(websling, np.array([[1000,5000],[0.6,0.9]]), np.array([37,5.0075]))
+    if statify:
+        pr.disable()
+        pstats.Stats(pr).print_stats()
--- ThreeDsim.py
+++ ThreeDsim.py
@@ -6,7 +6,7 @@ from scipy.integrate import ode
 from time import clock, sleep
 from scipy.optimize import minimize
 from scipy.optimize import fmin_slsqp
-
+import Spiderman as Spidey
 from matplotlib import pyplot
 
 import math
@@ -59,48 +59,22 @@ def switchingIteration(t, v, parameters, slingingRule):
     #If the sim is a stretchy string
     if d < 0:
         d = 0
-    dvdt = parameters.gravity - d*parameters.k*(r/la.norm(r))/parameters.mass - dragVector(velocity, parameters)/parameters.mass
+    dvdt = parameters.gravity - d*parameters.k*(r/la.norm(r))/parameters.mass + dragVector(velocity, parameters)/parameters.mass
     #print dragVector(velocity, parameters)/parameters.mass
     out = np.concatenate((drdt, dvdt))
     return out
 
 def dragVector(velocity, parameters):
-    dragParameter = lambda x: 0.5*parameters.airDensity*(x**2)*parameters.dragCoefficient*parameters.area
+    dragParameter = lambda x: -0.5*parameters.airDensity*abs(x)*x*parameters.dragCoefficient*parameters.area
     drag = np.array([(dragParameter(direction)) for direction in velocity])
     return drag
-
-def minimizeWebEnergyLost(x, parameters):
-    """values = [theta, velocity]"""
-    firstTerm = lambda values: (x * values[1]**2 / 2.0)
-    sqrtTerm = lambda values: np.sqrt((parameters.gravity[1]**2 * x**2) / (4 * values[1]**4 * np.cos(values[0])**4) + 1)
-    secondTerm = lambda values: (values[1]**4 * np.cos(values[0])**2) / parameters.gravity[1]
-    arcsinhTerm = lambda values: np.arcsinh((parameters.gravity[1] * x) / (2 * values[1]**2 * np.cos(values[0])**2))
-    costFunction = lambda values: firstTerm(values)*sqrtTerm(values)+secondTerm(values)*arcsinhTerm(values)
-    bounds = ((0,(math.pi/2.0)),(0,parameters.maxSlingSpeed))
-
-    leftTerm = lambda values: ((-parameters.gravity[1]/2)*parameters.distanceToWall**2)/((values[1]**2)*(math.cos(values[0])))
-    fullConstraint = lambda values: leftTerm(values)+(math.tan(0)*parameters.distanceToWall)-parameters.heightChange
-    cons = ({'type': 'eq', 'fun': lambda values: fullConstraint(values)})
-    #ineq means not equals to zero. eq is equal to zero
-    #return costFunction
-    minimum = minimize(costFunction, (pi/4, 100), method="SLSQP", bounds=bounds, constraints=cons)
-    return minimum
 
 #A function that will update the vpython sim
 def visualize(vals, parameters):
     """Updates the vpython sim with current values"""
     r = vals[0:3]
     velocity = vals[3:]
-    parameters.rod.axis=r
-    parameters.ball.pos=parameters.rod.pos + r
-    # Printing the current total energy
-    test = (parameters.mass/2 * la.norm(velocity)**2,
-            0 - parameters.mass * np.dot(parameters.gravity,r), 0)
-    parameters.energyTracker.append([parameters.mass/2 * la.norm(velocity)**2,
-            0 - parameters.mass * np.dot(parameters.gravity,parameters.rod.pos + r), 0])
-    if la.norm(r)-parameters.equilibriumLength > 0:
-        parameters.energyTracker[-1][2] = parameters.k/2*(la.norm(r)-parameters.equilibriumLength)**2
-    parameters.energyTracker[-1].append(sum(parameters.energyTracker[-1]))
+    parameters.energyTracker.append(Spidey.energyCalculator(velocity,r,parameters))
 
 
 def initVisualization(r0, parameters):
